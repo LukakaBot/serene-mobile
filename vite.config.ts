d93b9cd0f5e7110354fcfe45345b5a904d09b746
Lokaka <monishine_code@163.com>
2025-02-02 21:40:15 +0800

    feat: 配置自动导入等工具

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import Components from '@uni-helper/vite-plugin-uni-components';
+import UniManifest from '@uni-helper/vite-plugin-uni-manifest';
+import UniPages from '@uni-helper/vite-plugin-uni-pages';
+import AutoImport from 'unplugin-auto-import/vite';
+import uniPolyfill from 'vite-plugin-uni-polyfill';
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => {
@@ -9,6 +14,18 @@ export default defineConfig(async () => {
     plugins: [
       uni(),
       UnoCSS(),
+      Components(),
+      UniManifest(),
+      UniPages({
+        dts: 'src/typings/uni-pages.d.ts',
+      }),
+      AutoImport({
+        imports: ['vue', 'uni-app'],
+        dts: 'src/typings/auto-imports.d.ts',
+        dirs: ['src/composables', 'src/stores', 'src/utils'],
+        vueTemplate: true,
+      }),
+      uniPolyfill(),
     ],
   }
 });
