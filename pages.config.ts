478b5367ff94efdc290af28c0aeae92808d941d4
Lukaka <monishine_code@163.com>
2025-02-05 10:26:13 +0800

    feat: pinia

--- pages.config.ts
+++ pages.config.ts
@@ -1,28 +1,38 @@
 import { defineUniPages } from '@uni-helper/vite-plugin-uni-pages';
 
 export default defineUniPages({
-  'easycom': {
-    "autoscan": true,
-    "custom": {
-      "^wd-(.*)": "wot-design-uni/components/wd-$1/wd-$1.vue"
-    }
-  },
-  "pages": [
-    {
-      "path": "pages/home/index",
-      "type": "home",
-      "style": {
-        "navigationBarTitleText": "首页"
-      },
-    },
-    {
-      "path": "pages/logs/index",
-      "type": "logs",
-      "style": {
-        "navigationBarTitleText": "日志"
-      },
-      "layout": false
-    }
-  ],
-  "subPackages": []
-})
+	easycom: {
+		autoscan: true,
+		custom: {
+			'^wd-(.*)': 'wot-design-uni/components/wd-$1/wd-$1.vue',
+		},
+	},
+	pages: [
+		{
+			path: 'pages/home/index',
+			type: 'home',
+			style: {
+				navigationBarTitleText: '首页',
+			},
+		},
+		{
+			path: 'pages/category/index',
+			type: 'category',
+			style: {},
+		},
+		{
+			path: 'pages/mine/index',
+			type: 'mine',
+			style: {},
+		},
+		{
+			path: 'pages/logs/index',
+			type: 'logs',
+			style: {
+				navigationBarTitleText: '日志',
+			},
+			layout: false,
+		},
+	],
+	subPackages: [],
+});
