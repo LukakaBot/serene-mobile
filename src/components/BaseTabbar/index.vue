4cf68b563f9cf48ed66506f274e308a72ccf597e
Lukaka <monishine_code@163.com>
2025-02-14 14:02:02 +0800

    feat: BaseTabbar 增加页面跳转

--- src/components/BaseTabbar/index.vue
+++ src/components/BaseTabbar/index.vue
@@ -1,7 +1,9 @@
 <template>
-	<wd-tabbar v-model="tabbar" @change="handleChangeTabbar">
+	<wd-tabbar
+		:model-value="tabbarStore.tabbarIndex"
+		@change="handleChangeTabbar"
+	>
 		<wd-tabbar-item
-			v-model="tabbar"
 			:title="tab.title"
 			:icon="tab.icon"
 			v-for="tab in tabs"
@@ -12,15 +14,11 @@
 </template>
 
 <script setup lang="ts">
-// import { useTabbarStore } from '@/store/modules/tabbar';
+import { useTabbarStore } from '@/store/modules/tabbar';
 
-// const tabbarStore = useTabbarStore();
+const tabbarStore = useTabbarStore();
 
 const router = useRouter();
-
-const currentPage = ref('home');
-
-const tabbar = ref(0);
 
 const tabs = ref([
 	{ index: 0, title: '首页', name: 'home', icon: 'home', route: 'home' },
@@ -29,10 +27,8 @@ const tabs = ref([
 ]);
 
 function handleChangeTabbar({ value }: { value: number }) {
-	console.log('value: ' + value);
-	console.log('tabbar: ' + tabbar.value);
-	console.log('currentPage: ' + currentPage.value);
-	console.log('tabs: ' + tabs.value[value].route);
+	tabbarStore.setTabbarIndex(value);
+	tabbarStore.setTabbarName(tabs.value[value].title);
 	router.push(`/pages/${tabs.value[value].route}/index`);
 }
 </script>
--- src/store/modules/tabbar/types.d.ts
+++ src/store/modules/tabbar/types.d.ts
@@ -1,5 +1,7 @@
 export interface TabbarState {
+  /** 当前tabbar索引 */
   tabbarIndex: number;
+  /** 当前tabbar名称 */
   tabbarName: string;
 };
 
