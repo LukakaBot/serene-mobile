a2a3e2359de4f11aa58eee0848aa0ccccec4d222
鲨鱼辣椒 <monishine_code@163.com>
2025-05-19 11:15:55 +0800

    feat: 添加分享功能

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import App from "./App.vue";
 import 'uno.css';
 import router from './router';
 import { setupPinia } from './store';
+import share from "./mixin/share";
 
 export function createApp() {
   const app = createSSRApp(App);
 
-
   app.use(router);
+  app.mixin(share);
   setupPinia(app);
 
   return {
---
+++ src/mixin/share/index.ts
@@ -0,0 +1,14 @@
+export default {
+  onShareAppMessage() {
+    return {
+      title: '自定义分享标题',
+      path: '/pages/home/index'
+    }
+  },
+  onShareTimeline() {
+    return {
+      title: '自定义分享标题',
+      path: '/pages/home/index'
+    }
+  }
+}
